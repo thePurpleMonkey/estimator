0b8a55e4f64e2e8ca5ad33f7c0a33f35837ec07e
Michael Humphrey <mhumphree@gmail.com>
2023-02-03 17:00:48 -0800

    Added project save to estimator page

--- invoice.js
+++ invoice.js
@@ -1,11 +1,19 @@
 "use strict";
 
+var current_project;
+
+var saved_projects = new Object();
+
 var configuration = {
 	hourlyRate: 30,
 	taxRate: 15,
 };
 
+// Toasts
+const savedProjectToast = new bootstrap.Toast(document.getElementById("saved-toast"));
+
 $(function() {
+	loadProjects();
 	loadConfiguration();
 	setDefaults();
 	$("input").change(recalculateProfit);
@@ -28,7 +36,13 @@ $(function() {
 	// Add button click handlers
 	$("#clear-button").click(clearForm);
 	$("#configuration-save-button").click(saveNewConfigs);
-	$("#invoice-button").click(serializeData);
+	$("#invoice-button").click(sendToInvoice);
+	$("#save-button").click(saveProject);
+	$("#project-save-button").click(modalSaveClick);
+	$("#project-name-input").keyup(modalSaveInputChanged)
+	$("#navbar-save-link").click(saveProject);
+	$("#navbar-load-link").click(showLoadProjectModal);
+	$("#project-load-button").click(loadProject);
 
 	$("#revenue-input").focus();
 });
@@ -46,19 +60,23 @@ function checkForEmptyMaterial() {
 	return result;
 }
 
-function addNewMaterial() {
+function addNewMaterial(name, quantity=1, cost) {
 	let row = $("<div>").addClass("row");
 
 	// Material name
 	let div = $("<div>").addClass("col-md-6 col-sm-12 col-12");
 	div.append($("<input>").addClass("form-control material-name").prop({
 		type: "text",
-		placeholder: "Name"
+		placeholder: "Name",
+		value: name
 	}).keyup(() => {
 		if (checkForEmptyMaterial()) {
 			addNewMaterial();
 		}
 	}));
+	if (name) {
+
+	}
 	row.append(div);
 
 	// Material quantity
@@ -66,7 +84,8 @@ function addNewMaterial() {
 	div.append($("<input>").addClass("form-control material-quantity").prop({
 		type: "number",
 		min: 0,
-		value: 1
+		step: 1,
+		value: quantity
 	}).change(recalculateProfit)).focus(function() {
 		$(this).select();
 	});
@@ -76,7 +95,8 @@ function addNewMaterial() {
 	div = $("<div>").addClass("col-md-2 col-sm-4 col-4");
 	div.append($("<input>").addClass("form-control material-cost").prop({
 		type: "number",
-		min: 0
+		min: 0,
+		value: cost
 	}).change(recalculateProfit));
 	row.append(div);
 
@@ -161,6 +181,24 @@ function recalculateProfit(e) {
 	$("#labor-input").val(laborCost.toFixed(2));
 }
 
+function loadProjects() {
+	let data = JSON.parse(localStorage.getItem("projects"));
+	console.log(data);
+
+	if (data) {
+		saved_projects = data;
+	}
+
+	// Populate load projects dialog
+	Object.entries(saved_projects).forEach(project => {
+		console.log(project);
+		let project_name = project[0];
+		console.log(project_name);
+		let option = $("<option>").attr("value", project_name).text(project_name);
+		$("#load-project-select").append(option);
+	});
+}
+
 function loadConfiguration() {
 	let savedConfig = JSON.parse(localStorage.getItem("configuration"));
 	if (savedConfig) {
@@ -211,9 +249,18 @@ function saveNewConfigs() {
 	$("#configurationModal").modal("hide");
 }
 
+function sendToInvoice() {
+	let estimate = serializeData();
+
+	console.log("Estimate data:");
+	console.log(estimate);
+	sessionStorage.setItem("invoiceData", JSON.stringify(estimate));
+	window.location = "/draft_invoice.html"
+}
+
 function serializeData() {
-	let result = new Object();
-	result.materials = [];
+	let estimate = new Object();
+	estimate.materials = [];
 
 	// Serialize materials
 	$("#materials-list").children().each((index, element) => {
@@ -226,17 +273,84 @@ function serializeData() {
 
 		console.log(material);
 
-		result.materials.push(material);
+		if (material.name) {
+			estimate.materials.push(material);
+		}
 	});
 
-	result.total = $("#revenue-input").val();
-	result.totalHours = $("#hours-input").val();
-	result.hourlyRate = $("#hourly-rate-input").val();
-	result.laborCost = $("#labor-input").val();
-	result.materialsCost = calculateMaterialsCost();
+	estimate.name = $("#name-input").val();
+	estimate.total = $("#revenue-input").val();
+	estimate.totalHours = $("#hours-input").val();
+	estimate.hourlyRate = $("#hourly-rate-input").val();
+	estimate.laborCost = $("#labor-input").val();
+	estimate.taxRate = $("#tax-input").val();
+	estimate.materialsCost = calculateMaterialsCost();
 
-	console.log("Invoice data:");
-	console.log(result);
-	sessionStorage.setItem("invoiceData", JSON.stringify(result));
-	window.location = "/draft_invoice.html"
+	return estimate;
+}
+
+function saveProject() {
+	if (!current_project) {
+		// Prompt user to select a name for this project
+		$("#save-modal").modal("show");
+		return;
+	}
+
+	// Save project with given name
+	console.log("Saving project");
+	let project = serializeData();
+	console.log(project);
+	saved_projects[current_project] = project;
+	localStorage.setItem("projects", JSON.stringify(saved_projects));
+	console.log(saved_projects);
+	
+	// Show confirmation toast
+	savedProjectToast.show();
+}
+
+function modalSaveClick() {
+	let name = $("#project-name-input").val();
+	console.log(name);
+	if (name) {
+		current_project = name;
+		saveProject();
+		$("#project-name").text(current_project);
+	}
+	$("#save-modal").modal("hide");
+}
+
+function modalSaveInputChanged() {
+	if ($("#project-name-input").val()) {
+		$("#project-save-button").removeAttr("disabled");
+	} else {
+		$("#project-save-button").attr("disabled", true);
+	}
+}
+
+function showLoadProjectModal() {
+	$("#load-modal").modal("show");
+}
+
+function loadProject() {
+	let project_name = $("#load-project-select").val();
+	console.log("Loading project: " + project_name);
+	$("#project-name").text(project_name);
+	let project = saved_projects[project_name];
+	current_project = project_name
+	console.log(project);
+
+	$("#revenue-input").val(project.total);
+	// Materials
+	$("#materials-list").empty();
+	project.materials.forEach(material => {
+		addNewMaterial(material.name, material.quantity, material.unitCost);
+	});
+	addNewMaterial(); // Add a blank material at the end
+
+	$("#hours-input").val(project.totalHours);
+	$("#hourly-rate-input").val(project.hourlyRate);
+	$("#tax-input").val(project.taxRate);
+
+	recalculateProfit();
+	$("#load-modal").modal("hide");
 }
